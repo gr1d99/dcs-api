3de0362f98155bc6f0688bd855253930d2953c66
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-09-26 17:33:51 +0300

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, :password_digest, presence: true
+  validates :email, uniqueness: true
+end
